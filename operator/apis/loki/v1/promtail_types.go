e2bd2eaf0a2f5f7d954f841ab2ae20acff10c423
Jeong Beom Hong <hjeongb0320@naver.com>
2024-09-15 22:24:05 +0900

    feat: complete making promtail crd and controller

--- operator/apis/loki/v1/promtail_types.go
+++ operator/apis/loki/v1/promtail_types.go
@@ -4,234 +4,229 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // PromtailSpec defines the desired state of Promtail
 type PromtailSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
 	// +optional
-	Config PromtailConfig `json:"promtail_config"`
+	Config PromtailConfig `json:"promtail_config" yaml:"promtail_config"`
 }
 
 // PromtailConfig defines the configuration of the promtail deployment
 type PromtailConfig struct {
 	// +optional
-	Server ServerConfig `json:"server,omitempty"`
+	Server ServerConfig `json:"server,omitempty" yaml:"server,omitempty"`
 
 	// +optional
-	Clients []ClientConfig `json:"clients,omitempty"`
+	Clients []ClientConfig `json:"clients,omitempty" yaml:"clients,omitempty"`
 
 	// +optional
-	Positions PositionsConfig `json:"positions,omitempty"`
+	Positions PositionsConfig `json:"positions,omitempty" yaml:"positions,omitempty"`
 
 	// +optional
-	TargetConfig TargetConfigConfig `json:"target_config,omitempty"`
+	TargetConfig TargetConfigConfig `json:"target_config,omitempty" yaml:"target_config,omitempty"`
 
 	// +optional
-	ScrapeConfigs []ScrapeConfigsConfig `json:"scrape_configs,omitempty"`
+	ScrapeConfigs []ScrapeConfigsConfig `json:"scrape_configs,omitempty" yaml:"scrape_configs,omitempty"`
 }
 
 type ServerConfig struct {
-	Disable                   bool   `json:"disable,omitempty"`
-	ProfilingEnabled           bool   `json:"profiling_enabled,omitempty"`
-	HttpListenAddress          string `json:"http_listen_address,omitempty"`
-	HttpListenPort             int    `json:"http_listen_port,omitempty"`
-	GrpcListenAddress          string `json:"grpc_listen_address,omitempty"`
-	GrpcListenPort             int    `json:"grpc_listen_port,omitempty"`
-	RegisterInstrumentation    bool   `json:"register_instrumentation,omitempty"`
-	GracefulShutdownTimeout    string `json:"graceful_shutdown_timeout,omitempty"`
-	HttpServerReadTimeout      string `json:"http_server_read_timeout,omitempty"`
-	HttpServerWriteTimeout     string `json:"http_server_write_timeout,omitempty"`
-	HttpServerIdleTimeout      string `json:"http_server_idle_timeout,omitempty"`
-	GrpcServerMaxRecvMsgSize   int    `json:"grpc_server_max_recv_msg_size,omitempty"`
-	GrpcServerMaxSendMsgSize   int    `json:"grpc_server_max_send_msg_size,omitempty"`
-	GrpcServerMaxConcurrentStreams int `json:"grpc_server_max_concurrent_streams,omitempty"`
-	LogLevel                   string `json:"log_level,omitempty"`
-	HttpPathPrefix             string `json:"http_path_prefix,omitempty"`
-	HealthCheckTarget          bool   `json:"health_check_target,omitempty"`
-	EnableRuntimeReload        bool   `json:"enable_runtime_reload,omitempty"`
+	Disable                    bool   `json:"disable,omitempty" yaml:"disable,omitempty"`
+	ProfilingEnabled           bool   `json:"profiling_enabled,omitempty" yaml:"profiling_enabled,omitempty"`
+	HttpListenAddress          string `json:"http_listen_address,omitempty" yaml:"http_listen_address,omitempty"`
+	HttpListenPort             int    `json:"http_listen_port,omitempty" yaml:"http_listen_port,omitempty"`
+	GrpcListenAddress          string `json:"grpc_listen_address,omitempty" yaml:"grpc_listen_address,omitempty"`
+	GrpcListenPort             int    `json:"grpc_listen_port,omitempty" yaml:"grpc_listen_port,omitempty"`
+	RegisterInstrumentation    bool   `json:"register_instrumentation,omitempty" yaml:"register_instrumentation,omitempty"`
+	GracefulShutdownTimeout    string `json:"graceful_shutdown_timeout,omitempty" yaml:"graceful_shutdown_timeout,omitempty"`
+	HttpServerReadTimeout      string `json:"http_server_read_timeout,omitempty" yaml:"http_server_read_timeout,omitempty"`
+	HttpServerWriteTimeout     string `json:"http_server_write_timeout,omitempty" yaml:"http_server_write_timeout,omitempty"`
+	HttpServerIdleTimeout      string `json:"http_server_idle_timeout,omitempty" yaml:"http_server_idle_timeout,omitempty"`
+	GrpcServerMaxRecvMsgSize   int    `json:"grpc_server_max_recv_msg_size,omitempty" yaml:"grpc_server_max_recv_msg_size,omitempty"`
+	GrpcServerMaxSendMsgSize   int    `json:"grpc_server_max_send_msg_size,omitempty" yaml:"grpc_server_max_send_msg_size,omitempty"`
+	GrpcServerMaxConcurrentStreams int `json:"grpc_server_max_concurrent_streams,omitempty" yaml:"grpc_server_max_concurrent_streams,omitempty"`
+	LogLevel                   string `json:"log_level,omitempty" yaml:"log_level,omitempty"`
+	HttpPathPrefix             string `json:"http_path_prefix,omitempty" yaml:"http_path_prefix,omitempty"`
+	HealthCheckTarget          bool   `json:"health_check_target,omitempty" yaml:"health_check_target,omitempty"`
+	EnableRuntimeReload        bool   `json:"enable_runtime_reload,omitempty" yaml:"enable_runtime_reload,omitempty"`
 }
 
 type ClientConfig struct {
 	// Loki의 HTTP URL
 	// +optional
-	URL string `json:"url"`
+	URL string `json:"url" yaml:"url"`
 
 	// HTTP 헤더 설정
 	// +optional
-	Headers map[string]string `json:"headers,omitempty"`
+	Headers map[string]string `json:"headers,omitempty" yaml:"headers,omitempty"`
 
 	// 테넌트 ID
 	// +optional
-	TenantID string `json:"tenant_id,omitempty"`
+	TenantID string `json:"tenant_id,omitempty" yaml:"tenant_id,omitempty"`
 
 	// 최대 대기 시간
 	// +optional
-	BatchWait string `json:"batchwait,omitempty"`
+	BatchWait string `json:"batchwait,omitempty" yaml:"batchwait,omitempty"`
 
 	// 최대 배치 크기
 	// +optional
-	BatchSize int `json:"batchsize,omitempty"`
+	BatchSize int `json:"batchsize,omitempty" yaml:"batchsize,omitempty"`
 
 	// Basic Auth 설정
 	// +optional
-	BasicAuth *BasicAuthConfig `json:"basic_auth,omitempty"`
+	BasicAuth *BasicAuthConfig `json:"basic_auth,omitempty" yaml:"basic_auth,omitempty"`
 
 	// OAuth2 설정
 	// +optional
-	OAuth2 *OAuth2Config `json:"oauth2,omitempty"`
+	OAuth2 *OAuth2Config `json:"oauth2,omitempty" yaml:"oauth2,omitempty"`
 
 	// Bearer Token 설정
 	// +optional
-	BearerToken      string `json:"bearer_token,omitempty"`
-	BearerTokenFile  string `json:"bearer_token_file,omitempty"`
+	BearerToken     string `json:"bearer_token,omitempty" yaml:"bearer_token,omitempty"`
+	BearerTokenFile string `json:"bearer_token_file,omitempty" yaml:"bearer_token_file,omitempty"`
 
 	// 프록시 설정
 	// +optional
-	ProxyURL string `json:"proxy_url,omitempty"`
+	ProxyURL string `json:"proxy_url,omitempty" yaml:"proxy_url,omitempty"`
 
 	// TLS 설정
 	// +optional
-	TLSConfig *TLSConfig `json:"tls_config,omitempty"`
+	TLSConfig *TLSConfig `json:"tls_config,omitempty" yaml:"tls_config,omitempty"`
 
 	// 백오프 설정
 	// +optional
-	BackoffConfig *BackoffConfig `json:"backoff_config,omitempty"`
+	BackoffConfig *BackoffConfig `json:"backoff_config,omitempty" yaml:"backoff_config,omitempty"`
 
 	// Rate Limit 관련 설정
 	// +optional
-	DropRateLimitedBatches bool `json:"drop_rate_limited_batches,omitempty"`
+	DropRateLimitedBatches bool `json:"drop_rate_limited_batches,omitempty" yaml:"drop_rate_limited_batches,omitempty"`
 
 	// 외부 레이블 설정
 	// +optional
-	ExternalLabels map[string]string `json:"external_labels,omitempty"`
+	ExternalLabels map[string]string `json:"external_labels,omitempty" yaml:"external_labels,omitempty"`
 
 	// 요청 타임아웃
 	// +optional
-	Timeout string `json:"timeout,omitempty"`
+	Timeout string `json:"timeout,omitempty" yaml:"timeout,omitempty"`
 }
 
 type BasicAuthConfig struct {
 	// +optional
-	Username     string `json:"username,omitempty"`
+	Username string `json:"username,omitempty" yaml:"username,omitempty"`
 
 	// +optional
-	Password     string `json:"password,omitempty"`
+	Password string `json:"password,omitempty" yaml:"password,omitempty"`
 
 	// +optional
-	PasswordFile string `json:"password_file,omitempty"`
+	PasswordFile string `json:"password_file,omitempty" yaml:"password_file,omitempty"`
 }
 
 type OAuth2Config struct {
 	// +optional
-	ClientID         string            `json:"client_id,omitempty"`
+	ClientID string `json:"client_id,omitempty" yaml:"client_id,omitempty"`
 
 	// +optional
-	ClientSecret     string            `json:"client_secret,omitempty"`
+	ClientSecret string `json:"client_secret,omitempty" yaml:"client_secret,omitempty"`
 
 	// +optional
-	ClientSecretFile string            `json:"client_secret_file,omitempty"`
+	ClientSecretFile string `json:"client_secret_file,omitempty" yaml:"client_secret_file,omitempty"`
 
 	// +optional
-	Scopes           []string          `json:"scopes,omitempty"`
+	Scopes []string `json:"scopes,omitempty" yaml:"scopes,omitempty"`
 
 	// +optional
-	TokenURL         string            `json:"token_url,omitempty"`
+	TokenURL string `json:"token_url,omitempty" yaml:"token_url,omitempty"`
 
 	// +optional
-	EndpointParams   map[string]string `json:"endpoint_params,omitempty"`
+	EndpointParams map[string]string `json:"endpoint_params,omitempty" yaml:"endpoint_params,omitempty"`
 }
 
 type TLSConfig struct {
 	// +optional
-	CAFile             string `json:"ca_file,omitempty"`
+	CAFile string `json:"ca_file,omitempty" yaml:"ca_file,omitempty"`
 
 	// +optional
-	CertFile           string `json:"cert_file,omitempty"`
+	CertFile string `json:"cert_file,omitempty" yaml:"cert_file,omitempty"`
 
 	// +optional
-	KeyFile            string `json:"key_file,omitempty"`
+	KeyFile string `json:"key_file,omitempty" yaml:"key_file,omitempty"`
 
 	// +optional
-	ServerName         string `json:"server_name,omitempty"`
+	ServerName string `json:"server_name,omitempty" yaml:"server_name,omitempty"`
 
 	// +optional
-	InsecureSkipVerify bool   `json:"insecure_skip_verify,omitempty"`
+	InsecureSkipVerify bool `json:"insecure_skip_verify,omitempty" yaml:"insecure_skip_verify,omitempty"`
 }
 
 type BackoffConfig struct {
 	// +optional
-	MinPeriod   string `json:"min_period,omitempty"`
+	MinPeriod string `json:"min_period,omitempty" yaml:"min_period,omitempty"`
 
 	// +optional
-	MaxPeriod   string `json:"max_period,omitempty"`
+	MaxPeriod string `json:"max_period,omitempty" yaml:"max_period,omitempty"`
 
 	// +optional
-	MaxRetries  int    `json:"max_retries,omitempty"`
+	MaxRetries int `json:"max_retries,omitempty" yaml:"max_retries,omitempty"`
 }
 
 type PositionsConfig struct {
 	// +optional
-	Filename          string `json:"filename,omitempty"`
+	Filename string `json:"filename,omitempty" yaml:"filename,omitempty"`
 
 	// +optional
-	SyncPeriod        string `json:"sync_period,omitempty"`
+	SyncPeriod string `json:"sync_period,omitempty" yaml:"sync_period,omitempty"`
 
 	// +optional
-	IgnoreInvalidYaml bool `json:"ignore_invalid_yaml,omitempty"`
+	IgnoreInvalidYAML bool `json:"ignore_invalid_yaml,omitempty" yaml:"ignore_invalid_yaml,omitempty"`
 }
 
 type TargetConfigConfig struct {
 	// +optional
-	SyncPeriod string `json:"sync_period,omitempty"`
+	SyncPeriod string `json:"sync_period,omitempty" yaml:"sync_period,omitempty"`
 }
 
 type ScrapeConfigsConfig struct {
 	// +optional
-	JobName             string               `json:"job_name"`
+	JobName string `json:"job_name" yaml:"job_name"`
 
-	KubernetesSDConfigs []KubernetesSDConfig `json:"kubernetes_sd_configs,omitempty"`
+	KubernetesSDConfigs []KubernetesSDConfig `json:"kubernetes_sd_configs,omitempty" yaml:"kubernetes_sd_configs,omitempty"`
 
-	PipelineStages      []PipelineStage      `json:"pipeline_stages,omitempty"` 
+	PipelineStages []PipelineStage `json:"pipeline_stages,omitempty" yaml:"pipeline_stages,omitempty"`
 
-	RelabelConfigs      []RelabelConfigsConfig      `json:"relabel_configs,omitempty"`
+	RelabelConfigs []RelabelConfigsConfig `json:"relabel_configs,omitempty" yaml:"relabel_configs,omitempty"`
 }
 
 type KubernetesSDConfig struct {
 	// +optional
-	Role string `json:"role"`
+	Role string `json:"role" yaml:"role"`
 }
 
+type EmptyStruct struct{}
+
 type PipelineStage struct {
-	Docker bool `json:"docker,omitempty"`
-	Cri    bool `json:"cri,omitempty"`
+	Docker *EmptyStruct `json:"docker,omitempty" yaml:"docker,omitempty"`
+	Cri    *EmptyStruct `json:"cri,omitempty" yaml:"cri,omitempty"`
 }
 
 type RelabelConfigsConfig struct {
-
 	// Their content is concatenated using the configured separator.
-	SourceLabels []string `json:"source_labels,omitempty"` // 기본값 없음
+	SourceLabels []string `json:"source_labels,omitempty" yaml:"source_labels,omitempty"`
 
 	// Separator placed between concatenated source label values.
-	Separator string `json:"separator,omitempty"` // default = ";"
+	Separator string `json:"separator,omitempty" yaml:"separator,omitempty"`
 
 	// TargetLabel is the label to which the resulting value is written in a replace action.
-	TargetLabel string `json:"target_label,omitempty"` // 기본값 없음
+	TargetLabel string `json:"target_label,omitempty" yaml:"target_label,omitempty"`
 
 	// Regex is the regular expression against which the extracted value is matched.
-	Regex string `json:"regex,omitempty"` // default = "(.*)"
+	Regex string `json:"regex,omitempty" yaml:"regex,omitempty"`
 
 	// Modulus to take of the hash of the source label values.
-	Modulus uint64 `json:"modulus,omitempty"` // 기본값 없음
+	Modulus uint64 `json:"modulus,omitempty" yaml:"modulus,omitempty"`
 
 	// Replacement is the value against which a regex replace is performed if the regular expression matches.
-	Replacement string `json:"replacement,omitempty"` // default = "$1"
+	Replacement string `json:"replacement,omitempty" yaml:"replacement,omitempty"`
 
 	// Action to perform based on regex matching.
-	Action string `json:"action,omitempty"` // default = "replace"
+	Action string `json:"action,omitempty" yaml:"action,omitempty"`
 }
 
 // PromtailStatus defines the observed state of Promtail
--- operator/controllers/loki/promtail_controller.go
+++ operator/controllers/loki/promtail_controller.go
@@ -6,9 +6,14 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	"gopkg.in/yaml.v2"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	lokiv1 "github.com/grafana/loki/operator/apis/loki/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
 )
 
 // PromtailReconciler reconciles a Promtail object
@@ -31,9 +36,33 @@ type PromtailReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.4/pkg/reconcile
 func (r *PromtailReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	// Promtail 객체 가져오기
+	promtail := &lokiv1.Promtail{}
+	err := r.Get(ctx, req.NamespacedName, promtail)
+	if err != nil {
+			return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
 
-	// TODO(user): your logic here
+	// ConfigMap 생성 또는 업데이트
+	if err := r.createOrUpdateConfigMap(ctx, promtail); err != nil {
+			return ctrl.Result{}, err
+	}
+
+	// DaemonSet 생성 또는 업데이트
+	if err := r.createOrUpdateDaemonSet(ctx, promtail); err != nil {
+			return ctrl.Result{}, err
+	}
+
+	// RBAC 리소스 생성 또는 업데이트
+	if err := r.createOrUpdateClusterRole(ctx); err != nil {
+			return ctrl.Result{}, err
+	}
+	if err := r.createOrUpdateServiceAccount(ctx, promtail.Namespace); err != nil {
+			return ctrl.Result{}, err
+	}
+	if err := r.createOrUpdateClusterRoleBinding(ctx, promtail.Namespace); err != nil {
+			return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
 }
@@ -43,4 +72,241 @@ func (r *PromtailReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&lokiv1.Promtail{}).
 		Complete(r)
+}
+
+func (r *PromtailReconciler) createOrUpdateConfigMap(ctx context.Context, promtail *lokiv1.Promtail) error {
+	// PromtailSpec을 YAML로 변환
+	promtailConfigYAML, err := generatePromtailConfigYAML(promtail.Spec.Config)
+	if err != nil {
+			return err
+	}
+
+	// ConfigMap 생성 또는 업데이트
+	configMap := &corev1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+					Name:      "promtail-config",
+					Namespace: promtail.Namespace,
+			},
+			Data: map[string]string{
+					"promtail.yaml": promtailConfigYAML,
+			},
+	}
+
+	// ConfigMap이 이미 존재하는지 확인
+	existingConfigMap := &corev1.ConfigMap{}
+	err = r.Get(ctx, client.ObjectKey{Name: configMap.Name, Namespace: configMap.Namespace}, existingConfigMap)
+	if err == nil {
+			// 존재하면 업데이트
+			configMap.ResourceVersion = existingConfigMap.ResourceVersion
+			return r.Update(ctx, configMap)
+	} else {
+			// 존재하지 않으면 생성
+			return r.Create(ctx, configMap)
+	}
+}
+
+// func generatePromtailConfigYAML(config lokiv1.PromtailConfig) (string, error) {
+// 	// PromtailConfig 구조체를 맵으로 변환
+// 	configMap := map[string]interface{}{
+// 			"server":         config.Server,
+// 			"clients":        config.Clients,
+// 			"positions":      config.Positions,
+// 			"target_config":  config.TargetConfig,
+// 			"scrape_configs": config.ScrapeConfigs,
+// 	}
+
+// 	// YAML로 직렬화
+// 	yamlData, err := yaml.Marshal(configMap)
+// 	if err != nil {
+// 			return "", err
+// 	}
+
+// 	return string(yamlData), nil
+// }
+
+func generatePromtailConfigYAML(config lokiv1.PromtailConfig) (string, error) {
+	yamlData, err := yaml.Marshal(&config)
+	if err != nil {
+			return "", err
+	}
+	return string(yamlData), nil
+}
+
+func (r *PromtailReconciler) createOrUpdateDaemonSet(ctx context.Context, promtail *lokiv1.Promtail) error {
+	daemonSet := &appsv1.DaemonSet{
+			ObjectMeta: metav1.ObjectMeta{
+					Name:      "promtail-daemonset",
+					Namespace: promtail.Namespace,
+			},
+			Spec: appsv1.DaemonSetSpec{
+					Selector: &metav1.LabelSelector{
+							MatchLabels: map[string]string{
+									"name": "promtail",
+							},
+					},
+					Template: corev1.PodTemplateSpec{
+							ObjectMeta: metav1.ObjectMeta{
+									Labels: map[string]string{
+											"name": "promtail",
+									},
+							},
+							Spec: corev1.PodSpec{
+									ServiceAccountName: "promtail-serviceaccount",
+									Containers: []corev1.Container{
+											{
+													Name:  "promtail-container",
+													Image: "grafana/promtail",
+													Args: []string{
+															"-config.file=/etc/promtail/promtail.yaml",
+													},
+													Env: []corev1.EnvVar{
+															{
+																	Name: "HOSTNAME",
+																	ValueFrom: &corev1.EnvVarSource{
+																			FieldRef: &corev1.ObjectFieldSelector{
+																					FieldPath: "spec.nodeName",
+																			},
+																	},
+															},
+													},
+													VolumeMounts: []corev1.VolumeMount{
+															{
+																	Name:      "logs",
+																	MountPath: "/var/log",
+															},
+															{
+																	Name:      "promtail-config",
+																	MountPath: "/etc/promtail",
+															},
+															{
+																	Name:      "varlibdockercontainers",
+																	MountPath: "/var/lib/docker/containers",
+																	ReadOnly:  true,
+															},
+													},
+											},
+									},
+									Volumes: []corev1.Volume{
+											{
+													Name: "logs",
+													VolumeSource: corev1.VolumeSource{
+															HostPath: &corev1.HostPathVolumeSource{
+																	Path: "/var/log",
+															},
+													},
+											},
+											{
+													Name: "varlibdockercontainers",
+													VolumeSource: corev1.VolumeSource{
+															HostPath: &corev1.HostPathVolumeSource{
+																	Path: "/var/lib/docker/containers",
+															},
+													},
+											},
+											{
+													Name: "promtail-config",
+													VolumeSource: corev1.VolumeSource{
+															ConfigMap: &corev1.ConfigMapVolumeSource{
+																	LocalObjectReference: corev1.LocalObjectReference{
+																			Name: "promtail-config",
+																	},
+															},
+													},
+											},
+									},
+							},
+					},
+			},
+	}
+
+	// DaemonSet이 이미 존재하는지 확인
+	existingDaemonSet := &appsv1.DaemonSet{}
+	err := r.Get(ctx, client.ObjectKey{Name: daemonSet.Name, Namespace: daemonSet.Namespace}, existingDaemonSet)
+	if err == nil {
+			// 존재하면 업데이트
+			daemonSet.ResourceVersion = existingDaemonSet.ResourceVersion
+			return r.Update(ctx, daemonSet)
+	} else {
+			// 존재하지 않으면 생성
+			return r.Create(ctx, daemonSet)
+	}
+}
+
+// ClusterRole 생성
+func (r *PromtailReconciler) createOrUpdateClusterRole(ctx context.Context) error {
+	clusterRole := &rbacv1.ClusterRole{
+			ObjectMeta: metav1.ObjectMeta{
+					Name: "promtail-clusterrole",
+			},
+			Rules: []rbacv1.PolicyRule{
+					{
+							APIGroups: []string{""},
+							Resources: []string{"nodes", "services", "pods"},
+							Verbs:     []string{"get", "watch", "list"},
+					},
+			},
+	}
+
+	existingClusterRole := &rbacv1.ClusterRole{}
+	err := r.Get(ctx, client.ObjectKey{Name: clusterRole.Name}, existingClusterRole)
+	if err == nil {
+			// 존재하면 업데이트
+			clusterRole.ResourceVersion = existingClusterRole.ResourceVersion
+			return r.Update(ctx, clusterRole)
+	} else {
+			// 존재하지 않으면 생성
+			return r.Create(ctx, clusterRole)
+	}
+}
+
+// ServiceAccount 생성
+func (r *PromtailReconciler) createOrUpdateServiceAccount(ctx context.Context, namespace string) error {
+	serviceAccount := &corev1.ServiceAccount{
+			ObjectMeta: metav1.ObjectMeta{
+					Name:      "promtail-serviceaccount",
+					Namespace: namespace,
+			},
+	}
+
+	existingSA := &corev1.ServiceAccount{}
+	err := r.Get(ctx, client.ObjectKey{Name: serviceAccount.Name, Namespace: namespace}, existingSA)
+	if err == nil {
+			// 존재하면 업데이트 필요 없음 (변경사항이 없는 경우)
+			return nil
+	} else {
+			// 존재하지 않으면 생성
+			return r.Create(ctx, serviceAccount)
+	}
+}
+
+// ClusterRoleBinding 생성
+func (r *PromtailReconciler) createOrUpdateClusterRoleBinding(ctx context.Context, namespace string) error {
+	clusterRoleBinding := &rbacv1.ClusterRoleBinding{
+			ObjectMeta: metav1.ObjectMeta{
+					Name: "promtail-clusterrolebinding",
+			},
+			Subjects: []rbacv1.Subject{
+					{
+							Kind:      "ServiceAccount",
+							Name:      "promtail-serviceaccount",
+							Namespace: namespace,
+					},
+			},
+			RoleRef: rbacv1.RoleRef{
+					Kind:     "ClusterRole",
+					Name:     "promtail-clusterrole",
+					APIGroup: "rbac.authorization.k8s.io",
+			},
+	}
+
+	existingCRB := &rbacv1.ClusterRoleBinding{}
+	err := r.Get(ctx, client.ObjectKey{Name: clusterRoleBinding.Name}, existingCRB)
+	if err == nil {
+			// 존재하면 업데이트
+			clusterRoleBinding.ResourceVersion = existingCRB.ResourceVersion
+			return r.Update(ctx, clusterRoleBinding)
+	} else {
+			// 존재하지 않으면 생성
+			return r.Create(ctx, clusterRoleBinding)
+	}
 }
